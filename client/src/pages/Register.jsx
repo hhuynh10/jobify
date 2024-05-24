98302dd239a437f0a2621f28ff1dd8c5085a9880
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-05-23 20:44:39 -0500

    study

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,10 +6,12 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
+  console.log(data);
   try {
     await customFetch.post("/auth/register", data);
     return redirect("/login");
   } catch (error) {
+    console.log(error);
     return error;
   }
 };
